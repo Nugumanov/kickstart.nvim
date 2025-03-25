3f44422773a8efb4903746ae5a1a2fd8185d4399
Advena <dream2web.group@gmail.com>
2025-03-25 06:28:20 +0400

    Add avante.nvim plugin with neo-tree integration

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -17,6 +17,82 @@ return {
   { 'knubie/vim-kitty-navigator' },
 
   {
+    'yetone/avante.nvim',
+    event = 'VeryLazy',
+    version = false,
+    build = 'make BUILD_FROM_SOURCE=true', -- TOP-LEVEL BUILD COMMAND
+    dependencies = { -- TOP-LEVEL DEPENDENCIES
+      'nvim-treesitter/nvim-treesitter',
+      'stevearc/dressing.nvim',
+      'nvim-lua/plenary.nvim',
+      'MunifTanjim/nui.nvim',
+      'echasnovski/mini.pick',
+      'nvim-telescope/telescope.nvim',
+      'hrsh7th/nvim-cmp',
+      'ibhagwan/fzf-lua',
+      'nvim-tree/nvim-web-devicons',
+      'zbirenbaum/copilot.lua',
+      {
+        'HakonHarnes/img-clip.nvim',
+        event = 'VeryLazy',
+        opts = {
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = { insert_mode = true },
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = { file_types = { 'markdown', 'Avante' } },
+        ft = { 'markdown', 'Avante' },
+      },
+    },
+    opts = {
+      provider = 'deepseek',
+      vendors = {
+        deepseek = {
+          __inherited_from = 'openai',
+          api_key_name = 'DEEPSEEK_API_KEY',
+          endpoint = 'https://api.deepseek.com',
+          model = 'deepseek-coder',
+        },
+      },
+      tools = {
+        web_search = {
+          enabled = true,
+          provider = 'tavily', -- or 'duckduckgo', 'bing', etc.
+        },
+        repository_actions = {
+          enabled = true,
+        },
+      },
+      git = {
+        enabled = true,
+        root_dir = vim.fn.getcwd(), -- or specify a custom path to your Git repositories
+      },
+    },
+    config = function(_, opts)
+      require('avante').setup(opts)
+      -- Keybindings
+      vim.keymap.set('n', '<leader>aw', '<cmd>AvanteWebSearch<CR>', { desc = '[A]vante [W]eb Search' })
+      vim.keymap.set('n', '<leader>ar', '<cmd>AvanteRepositoryView<CR>', { desc = '[A]vante [R]epository View' })
+    end,
+  },
+
+  -- required by avante.nvim to use a web-search
+  {
+    -- Make sure to set this up properly if you have lazy=true
+    'MeanderingProgrammer/render-markdown.nvim',
+    opts = {
+      file_types = { 'markdown', 'Avante' },
+    },
+    ft = { 'markdown', 'Avante' },
+  },
+
+  {
     'akinsho/toggleterm.nvim',
     version = '*',
     opts = {
