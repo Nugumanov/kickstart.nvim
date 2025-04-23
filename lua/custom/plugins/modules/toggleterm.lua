8c976010b7de388c2584aca20cf29ff6700b317c
Advena <dream2web.group@gmail.com>
2025-04-23 13:14:21 +0400

    Change nvim toggleterm config

--- lua/custom/plugins/modules/toggleterm.lua
+++ lua/custom/plugins/modules/toggleterm.lua
@@ -4,7 +4,28 @@ return {
   opts = {
     open_mapping = [[<c-\>]],
     autochdir = true,
+    direction = 'float',
+    winbar = {
+      enabled = false,
+    },
+    shade_terminals = true,
+    shading_factor = 50,
+    shading_ratio = 30,
     start_in_insert = true,
-    --direction = 'float',
+    float_opts = {
+      border = 'single',
+      width = 155,
+      height = 46,
+      row = 6,
+      col = 20,
+      winblend = 30,
+      --zindex = 999,
+    },
+    winbar = {
+      enabled = false,
+      name_formatter = function(term) --  term: Terminal
+        return term.name
+      end,
+    },
   },
 }
