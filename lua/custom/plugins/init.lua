80259ab783803444dd32e154adad6a57e7dce80f
Advena <dream2web.group@gmail.com>
2025-04-23 13:16:18 +0400

    Switch nvim focus.nvim with vimade plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,10 +2,11 @@ return {
   { import = 'custom/plugins/modules/vim-fugitive' },
   { import = 'custom/plugins/modules/avante' },
   { import = 'custom/plugins/modules/toggleterm' },
-  { import = 'custom/plugins/modules/focus' },
+  --{ import = 'custom/plugins/modules/focus' },
   { import = 'custom/plugins/modules/obsidian' },
   { import = 'custom/plugins/modules/oil' },
   --{ import = 'custom/plugins/modules/gitlab' },
+  { import = 'custom/plugins/modules/vimade' },
 
   { 'knubie/vim-kitty-navigator' },
 
