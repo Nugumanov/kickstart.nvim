6b87a644c29612216d2998728261355558bfdca6
Advena <dream2web.group@gmail.com>
2025-06-12 03:14:09 +0400

    Add mcphub nvim plugin config

--- lua/custom/plugins/modules/mcphub.lua
+++ lua/custom/plugins/modules/mcphub.lua
@@ -7,6 +7,18 @@ return {
   config = function()
     require('mcphub').setup {
       use_bundled_binary = true, -- Use local `mcp-hub` binary
+
+      port = 3000, -- Default hub port
+      config = vim.fn.expand '~/.config/mcphub/servers.json', -- Absolute path required
+
+      -- Optional customization
+      log = {
+        level = vim.log.levels.WARN, -- DEBUG, INFO, WARN, ERROR
+        to_file = true, -- Creates ~/.local/state/nvim/mcphub.log
+      },
+      on_ready = function()
+        vim.notify 'MCP Hub is online!'
+      end,
     }
   end,
 }
