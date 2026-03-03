1b47ce8cdbafa56c6c7c1e3dd3d492337a4de601
Advena <dream2web.group@gmail.com>
2026-03-03 14:53:38 +0400

    feat(theme): sync catppuccin flavour with kitty theme state file

--- lua/custom/themes/init.lua
+++ lua/custom/themes/init.lua
@@ -4,26 +4,46 @@ return {
     name = 'catppuccin',
     priority = 1000,
     config = function()
-      require('catppuccin').setup {
-        flavour = 'macchiato',
-        transparent_background = true,
-        integrations = {
-          cmp = true,
-          gitsigns = true,
-          treesitter = true,
-          neotree = true,
-          barbar = true,
-          copilot_vim = true,
-        },
-        dim_inactive = {
-          enabled = false,
-          shade = 'dark',
-          percentage = 0.05,
-        },
-      }
+      -- Read theme state from kitty toggle script
+      local function get_flavour()
+        local state_file = vim.fn.expand('~/.config/kitty/.theme')
+        if vim.fn.filereadable(state_file) == 1 then
+          local line = vim.fn.readfile(state_file)[1]
+          if line == 'light' then return 'latte' end
+        end
+        return 'macchiato' -- default: dark
+      end
 
-      vim.cmd.colorscheme 'catppuccin'
-      vim.cmd.hi 'Comment gui=none'
+      local function apply_theme()
+        local flavour = get_flavour()
+        require('catppuccin').setup {
+          flavour = flavour,
+          transparent_background = true,
+          integrations = {
+            cmp = true,
+            gitsigns = true,
+            treesitter = true,
+            neotree = true,
+            barbar = true,
+            copilot_vim = true,
+          },
+          dim_inactive = {
+            enabled = false,
+            shade = 'dark',
+            percentage = 0.05,
+          },
+        }
+        vim.cmd.colorscheme 'catppuccin'
+        vim.cmd.hi 'Comment gui=none'
+      end
+
+      apply_theme()
+
+      -- Re-apply theme when kitty's toggle-theme.sh switches while nvim is open
+      vim.api.nvim_create_autocmd('FocusGained', {
+        callback = apply_theme,
+        desc = 'Sync catppuccin flavour with kitty theme state file',
+      })
     end,
 
     init = function()
